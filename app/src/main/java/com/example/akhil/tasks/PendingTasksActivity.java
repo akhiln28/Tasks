9baad334ae9c62062bc62bfb07562220a347594c
akhiln28 <akhil28111997@gmail.com>
2017-07-12 17:31:37 +0530

    How to add clicklistener to the listview. And also i use just 2 types of list adapters (most of them made the app unfortunately stop) I don't know why they are stopping!

--- app/src/main/java/com/example/akhil/tasks/PendingTasksActivity.java
+++ app/src/main/java/com/example/akhil/tasks/PendingTasksActivity.java
@@ -2,9 +2,12 @@ package com.example.akhil.tasks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class PendingTasksActivity extends AppCompatActivity {
 
@@ -17,5 +20,12 @@ public class PendingTasksActivity extends AppCompatActivity {
         ListAdapter akhilAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
         ListView akhilListView = (ListView) findViewById(R.id.listview_pending_tasks);
         akhilListView.setAdapter(akhilAdapter);
+        akhilListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String name = String.valueOf(adapterView.getItemAtPosition(i));
+                Toast.makeText(PendingTasksActivity.this,name,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
